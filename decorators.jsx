10c45235c881e11d1228bb2338e5be21422d3503
Christophe Eymard <christophe.eymard@sales-way.com>
2015-10-22 23:35:53 +0200

    modification of the file structure

---
+++ decorators.jsx
@@ -0,0 +1,52 @@
+
+import {BindController} from './controllers/bind';
+import {o, Observable} from './observable';
+
+export function bind(obs, opts) {
+
+  if (!obs) return;
+
+  return function bindDecorator(node) {
+    let ctrl = new BindController(obs, opts);
+    node.addController(ctrl);
+  };
+
+}
+
+
+export function click(cbk) {
+
+    return function clickDecorator(node) {
+
+      node.once('dom-created', function () {
+        this.$node.addEventListener('click', cbk);
+      });
+
+    };
+
+}
+
+export function cls(obj) {
+
+  return function clsDecorator(node) {
+
+    node.once('dom-created', function () {
+      let clslist = this.$node.classList;
+
+      for (let cls in obj) {
+        let obs = obj[cls];
+        if (obs instanceof Observable) {
+          node.observe(obs, (val) => {
+            if (val) clslist.add(cls);
+            else clslist.remove(cls);
+          });
+        } else {
+          if (obs) clslist.add(cls);
+          else clslist.remove(cls);
+        }
+      }
+    });
+
+  }
+
+}
