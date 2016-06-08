b664da86ebca4d6cca146f5b3a24bfcaaeb3efc1
Christophe Eymard <christophe.eymard@sales-way.com>
2016-06-08 15:37:52 +0200

    better typing for If() and Match()

--- src/control.ts
+++ src/control.ts
@@ -1,12 +1,12 @@
 
-import {Atom, Appendable} from './atom'
+import {Atom, Appendable, ObservableAtom} from './atom'
 import {o, O, Observable} from './observable'
 
 export type ConditionalBuilder<T> = (a: T) => Appendable
 
-export function If<T>(cond: O<T>, fn: ConditionalBuilder<T>, fnelse?: ConditionalBuilder<T>): Observable<Appendable> {
+export function If<T>(cond: O<T>, fn: ConditionalBuilder<T>, fnelse?: ConditionalBuilder<T>): ObservableAtom<Appendable> {
   var obs: Observable<T> = o(cond)
-  return obs.tf(val => val ? fn(val) : (fnelse ? fnelse(val) : null))
+  return new ObservableAtom<Appendable>(obs.tf<Appendable>(val => val ? fn(val) : (fnelse ? fnelse(val) : null)))
   // return o(cond, (val: any) => val ? fn(val) : (fnelse ? fnelse(val) : null))
 }
 
@@ -15,17 +15,17 @@ export function Else<T>(fn: ConditionalBuilder<T>): ConditionalBuilder<T> { retu
 
 
 
-export function Match<T>(obj: O<T>, ...args: ConditionalBuilder<T>[]): Observable<Appendable> {
+export function Match<T>(obj: O<T>, ...args: ConditionalBuilder<T>[]): ObservableAtom<Appendable> {
   var obs: Observable<T> = o(obj)
 
-  return obs.tf((val: T) => {
+  return new ObservableAtom<Appendable>(obs.tf((val: T) => {
     let res: Appendable = null
     for (let i = 0; i < args.length; i++) {
       res = args[i](val)
       if (res) return res
     }
     return null
-  })
+  }))
 }
 
 
